dee575c6e836a4f70acdfa6907a5e3541e3d1c0d
shovalul <shovalul@campus.technion.ac.il>
2018-12-14 10:59:14 +0200

    frontend changes

---
+++ K-of-N-dapp/src/components/k-of-n-dapp.vue
@@ -0,0 +1,18 @@
+<template>
+ <metamask-data/>
+</template>
+<script>
+import MetamaskData from '@/components/metamask-data'
+export default {
+ name: 'k-of-n-dapp',
+ beforeCreate () {
+    console.log('registerWeb3 Action dispatched from k-of-n-dapp.vue')
+    this.$store.dispatch('registerWeb3')
+  },
+ components: {
+ 'metamask-data': MetamaskData
+ }
+}
+</script>
+<style scoped>
+</style>
--- K-of-N-dapp/src/router/index.js
+++ K-of-N-dapp/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import KofNDapp from '@/components/k-of-n-dapp'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'k-of-n-dapp',
+      component: KofNDapp
     }
   ]
 })
