affa18c034687b81c9d27c067743d143d54be098
davidstrouksf <david.strouk@barrel.network>
2019-02-18 20:44:35 +0200

    add "vue-notifications"

--- K-of-N-dapp/src/main.js
+++ K-of-N-dapp/src/main.js
@@ -1,15 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import { store } from './store/'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import { store } from './store/';
 import BootstrapVue from 'bootstrap-vue';
+import Notifications from 'vue-notification';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
+Vue.use(Notifications);
 
 Vue.config.productionTip = false;
 /* eslint-disable no-new */
